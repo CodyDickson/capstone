171afcd86d4e45de17d36eeac66656524dd1379c
Cody <54541144+CodyDickson@users.noreply.github.com>
2020-02-25 12:30:34 -0500

    Add files via upload

--- Testing-Calendar/calendar.php
+++ Testing-Calendar/calendar.php
@@ -1,2 +1,49 @@
 <?php
+$monthTracker = array(false, false, false, false, false, false, false, false, false, false, false, false);
+$monthList = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+$sql = "SELECT * FROM events ORDER BY EventStartDate";
+if ($result = mysqli_query($conn, $sql)) {
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table>";
+        while ($row = mysqli_fetch_array($result)) {
+            $month = date("m", strtotime($row['EventStartDate']));
+            if ($monthTracker[($month - 1)] == false) {
+                echo "<tr>";
+                    echo "<th colspan='2'><h1>".$monthList[($month - 1)]."</h1></th>";
+                echo "</tr>";
+                $monthTracker[($month - 1)] = true;
+            }
+            echo "<tr>";
+                echo "<td><h2>".$row['EventName']."</h2></td>";
+            echo "</tr>";
+            echo "<tr>";
+                $committeeName = CommitteeCheck($row["CommitteeID"]);
+                echo "<td><i>Hosted by the $committeeName Committee</i></td>";
+            echo "</tr>";
+            echo"<tr>";
+                echo "<td id='date'>Start Date: ".date('m/d/Y', strtotime($row['EventStartDate']))." - End Date: ".date('m/d/Y', strtotime($row['EventEndDate']))."</td>";
+            echo "</tr>";
+            echo "<tr>";
+                echo "<td>Location: ".$row['Location']."</td>";
+            echo "</tr>";
+            echo "<tr>";
+                echo "<td colspan='2'>".$row['EventDesc']."</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        mysqli_free_result($result);
+    } else {
+        echo "No records matching your query were found.";
+    }
+} else {
+    echo "ERROR: Could not execute $sql.".mysqli_error($link);
+}
+
+function CommitteeCheck($committeeID) {
+    if ($committeeID == 1) {
+        $name = "Fundraising";
+        return $name;
+    }
+}
 ?>
--- Testing-Calendar/index.php
+++ Testing-Calendar/index.php
@@ -1,39 +1,22 @@
 <?php
 error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
 ini_set('display_errors', 1);
-require_once 'calendar.php';
+require_once 'connection.php';
+$conn = OpenCon();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>   
-    <link href="style.css" type="text/css" rel="stylesheet" />
+    <link href="style2.css" rel="stylesheet" media="screen" type="text/css"/>
 </head>
 <body>
-<div>
-    <h1>FEBRUARY</h1>
-    <h2>Event Name</h2>
-    <h3>Start Date to End Date</h3>
-    <h3>Event Location</h3>
-    <h3>Hosted by Committee</h3>
-    <div>Description of the event.</div>
-    <div>----------</div>
-    <h1>MARCH</h1>
-    <h2>Event Name</h2>
-    <h3>Start Date to End Date</h3>
-    <h3>Event Location</h3>
-    <h3>Hosted by Committee</h3>
-    <div>Description of the event.</div>
-    <button>Edit</button><button>Delete</button>
-    <div>----------</div>
+<div id="mainTest">
+    <?php
+    require_once 'calendar.php';
+    ?>
 </div>
-<div>
-    <div>----------</div>
-    <button>New Event</button>
-    <div>----------</div>
-    <h1>FEBRUARY</h1>
-    <div>Event Name <button>Edit</button><button>Delete</button></div>
-    <div>Event Name <button>Edit</button><button>Delete</button></div>
-    <div>----------</div>
-</div>
+<?php
+CloseCon($conn);
+?>
 </body>
 </html>
